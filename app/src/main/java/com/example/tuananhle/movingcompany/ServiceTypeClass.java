b429c504ede14f482a801b677cd8dcaf72a61b6e
An Dy Le <smafun@outlook.com>
2018-01-18 13:24:47 +0100

    Add new ServiceTypeClass, new NewServiceTypeClass and new_activity_service_types.xml

---
+++ app/src/main/java/com/example/tuananhle/movingcompany/ServiceTypeClass.java
@@ -0,0 +1,18 @@
+package com.example.tuananhle.movingcompany;
+
+/**
+ * Created by tuananhle on 17.01.2018.
+ */
+
+public class ServiceTypeClass {
+    private String ServiceTypeId;
+    private String ServiceTypeName;
+
+    public String getServiceTypeId() {
+        return this.ServiceTypeId;
+    }
+
+    public String getServiceTypeName() {
+        return this.ServiceTypeName;
+    }
+}
--- app/src/main/java/com/example/tuananhle/movingcompany/ServiceTypesActivity.java
+++ app/src/main/java/com/example/tuananhle/movingcompany/ServiceTypesActivity.java
@@ -1,13 +1,41 @@
 package com.example.tuananhle.movingcompany;
+//import org.springframework.web.client.RestTemplate;
 
+import android.content.Intent;
+import android.net.http.HttpResponseCache;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpClientStack;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+//import org.springframework.web.client.RestTemplate;
 
 public class ServiceTypesActivity extends AppCompatActivity {
 
@@ -22,17 +50,55 @@ public class ServiceTypesActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(view.getContext(), NewServiceTypesActivity.class);
+                startActivity(intent);
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        // Instantiate the RequestQueue.
+
+    // Start testing
+     /*   try {
+            final String url = "http://rest-service.guides.spring.io/greeting";
+            RestTemplate restTemplate = new RestTemplate();
+            restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+            ServiceTypeClass greeting = restTemplate.getForObject(url, ServiceTypeClass.class);
+            //return greeting;
+        } catch (Exception e) {
+            Log.e("MainActivity", e.getMessage(), e);
+        }
+*/
+    /*    RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://localhost:5000/api/orderplacement";
+        StringRequest getRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.d("Response", response);
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("MC", "list customers");
+                    }
+                }
+        ) ;
+        queue.add(getRequest);
+*/
+        // End testing
         String[] myStringArray = new String[]{"Tuan", "Uwe"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, myStringArray);
         ListView listView = (ListView) findViewById(R.id.listview);
         listView.setAdapter(adapter);
     }
+
+
 
 }
