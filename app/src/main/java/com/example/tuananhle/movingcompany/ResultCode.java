8eca7c9474cdb51b83fddc7f0756590d4f7bb02f
An Dy Le <smafun@outlook.com>
2018-01-19 07:53:47 +0100

    Add help class

---
+++ app/src/main/java/com/example/tuananhle/movingcompany/ResultCode.java
@@ -0,0 +1,17 @@
+package com.example.tuananhle.movingcompany;
+
+/**
+ * Created by tuananhle on 18.01.2018.
+ */
+
+public class ResultCode {
+    public static final int ORDER_CREATE_SUCCESS = 1;
+    public static final int ORDER_UPDATE_SUCCESS = 2;
+    public static final int ORDER_DELETE_SUCCESS = 3;
+    public static final int CUSTOMER_CREATE_SUCCESS = 4;
+    public static final int CUSTOMERUPDATE_SUCCESS = 5;
+    public static final int CUSTOMER_DELETE_SUCCESS = 6;
+    public static final int SERVICETYPE_CREATE_SUCCESS = 7;
+    public static final int SERVICETYPE_UPDATE_SUCCESS = 8;
+    public static final int SERVICETYPE_DELETE_SUCCESS = 9;
+}
---
+++ app/src/main/java/com/example/tuananhle/movingcompany/ServiceUrl.java
@@ -0,0 +1,15 @@
+package com.example.tuananhle.movingcompany;
+
+import com.android.volley.toolbox.StringRequest;
+
+/**
+ * Created by tuananhle on 18.01.2018.
+ */
+
+public class ServiceUrl {
+    public static final String REST_SERVICE_URL = "http://localhost:5000/api/";
+    public static final String ORDER = REST_SERVICE_URL + "orderplacement";
+    public static final String CUSTOMER = REST_SERVICE_URL + "customer";
+    public static final String SERVICETYPES = REST_SERVICE_URL + "servicetypes";
+}
+
